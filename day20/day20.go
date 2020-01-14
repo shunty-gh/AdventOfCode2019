22a07ab33935e98646a5886b00825129cbcb19d0
Steven Hunt <sph@commandersoft.co.uk>
2020-01-14 09:48:23 +0000

    Update day20.go

--- day20/day20.go
+++ day20/day20.go
@@ -7,8 +7,9 @@ import (
 	"strings"
 )
 
-type location struct {
-	X, Y, Z int
+// GridRef : A 2D grid reference
+type GridRef struct {
+	X, Y int
 }
 
 type mazeContent struct {
@@ -16,12 +17,12 @@ type mazeContent struct {
 	PortalId string
 }
 
-type portal []location
-
 // Maze : A donut maze
 type Maze struct {
-	Map     map[location]mazeContent
-	Portals map[string]portal
+	Map                    map[GridRef]mazeContent
+	PortalIds              map[string][]GridRef
+	Portals                map[GridRef]string
+	MinX, MaxX, MinY, MaxY int
 }
 
 func main() {
@@ -31,46 +32,50 @@ func main() {
 		log.Fatal(err)
 	}
 	maze := buildMaze(input)
-	part1 := shortestPath(maze, maze.Portals["AA"][0], maze.Portals["ZZ"][0])
+	part1 := shortestPath(maze, maze.PortalIds["AA"][0], maze.PortalIds["ZZ"][0])
 	fmt.Println("Part 1: ", part1)
 }
 
-type locationDistance struct {
-	Loc  location
-	Dist int
+type locationToVisit struct {
+	Location        GridRef
+	Level, Distance int
 }
 
-func shortestPath(maze Maze, start location, target location) int {
-	moves := []location{{0, 1, 0}, {1, 0, 0}, {0, -1, 0}, {-1, 0, 0}}
+type visitedLocation struct {
+	X, Y, Z int
+}
+
+func shortestPath(maze Maze, start GridRef, target GridRef) int {
+	moves := []GridRef{{0, 1}, {1, 0}, {0, -1}, {-1, 0}}
 	result := 0
-	q := []locationDistance{locationDistance{start, 0}}
-	visited := make(map[location]int)
+	q := []locationToVisit{locationToVisit{start, 0, 0}}
+	visited := make(map[visitedLocation]bool)
 	for len(q) > 0 {
-		ld := q[0]
-		loc, dist := ld.Loc, ld.Dist
+		vl := q[0]
+		loc, lvl, dist := vl.Location, vl.Level, vl.Distance
 
 		// Check if we're on a portal
-		if mc := maze.Map[loc]; mc.PortalId != "" && mc.PortalId != "AA" {
-			port := maze.Portals[mc.PortalId]
+		if mc := maze.Map[loc]; mc.PortalId != "" && mc.PortalId != "AA" && mc.PortalId != "ZZ" {
+			port := maze.PortalIds[mc.PortalId]
 			if port[0] == loc {
-				q = append(q, locationDistance{port[1], dist + 1})
+				q = append(q, locationToVisit{port[1], 0, dist + 1})
 			} else {
-				q = append(q, locationDistance{port[0], dist + 1})
+				q = append(q, locationToVisit{port[0], 0, dist + 1})
 			}
 		}
 		for _, move := range moves {
-			nextloc := location{loc.X + move.X, loc.Y + move.Y, 0}
+			nextloc := GridRef{loc.X + move.X, loc.Y + move.Y}
 			if nextloc == target {
 				return dist + 1
 			}
-			if _, ok := visited[nextloc]; ok { // Skip if we've already visited
+			if _, ok := visited[visitedLocation{nextloc.X, nextloc.Y, lvl}]; ok { // Skip if we've already visited
 				continue
 			}
 
 			// Check it is a valid place to visit
 			if mc, ok := maze.Map[nextloc]; ok && mc.Content == '.' {
-				visited[nextloc] = dist + 1
-				q = append(q, locationDistance{Loc: nextloc, Dist: dist + 1})
+				visited[visitedLocation{nextloc.X, nextloc.Y, lvl}] = true
+				q = append(q, locationToVisit{nextloc, lvl, dist + 1})
 			}
 		}
 
@@ -86,45 +91,52 @@ func isPortalLetter(ch byte) bool {
 func buildMaze(input []string) Maze {
 	maxY := len(input)
 	maxX := len(input[2]) + 2
-	result := Maze{make(map[location]mazeContent), make(map[string]portal)}
+	result := Maze{
+		Map:       make(map[GridRef]mazeContent),
+		PortalIds: make(map[string][]GridRef),
+		Portals:   make(map[GridRef]string)}
+
 	for y, line := range input {
 		for x := 0; x < len(line); x++ {
 			c := line[x]
 			if c == '.' {
-				if _, ok := result.Map[location{x, y, 0}]; !ok {
-					result.Map[location{x, y, 0}] = mazeContent{Content: c}
+				if _, ok := result.Map[GridRef{x, y}]; !ok {
+					result.Map[GridRef{x, y}] = mazeContent{Content: c}
 				}
 			} else if isPortalLetter(c) {
 				newportal := false
 				var pkey string
-				var ploc location
+				var pref GridRef
 				// Vertical
 				if y+1 < maxY && x < len(input[y+1]) && isPortalLetter(input[y+1][x]) {
 					newportal = true
 					pkey = string(c) + string(input[y+1][x])
 					// Below or above?
 					if y+2 < maxY && x < len(input[y+2]) && input[y+2][x] == '.' {
-						ploc = location{x, y + 2, 0}
+						pref = GridRef{x, y + 2}
 					} else {
-						ploc = location{x, y - 1, 0}
+						pref = GridRef{x, y - 1}
 					}
 				} else if x+1 < maxX && isPortalLetter(input[y][x+1]) { // Horizontal
 					newportal = true
 					pkey = string(c) + string(input[y][x+1])
 					// Right or left
 					if x+2 < maxX && input[y][x+2] == '.' {
-						ploc = location{x + 2, y, 0}
+						pref = GridRef{x + 2, y}
 					} else {
-						ploc = location{x - 1, y, 0}
+						pref = GridRef{x - 1, y}
 					}
 				}
 				if newportal {
-					if p, ok := result.Portals[pkey]; ok {
-						result.Portals[pkey] = append(p, ploc)
+					if p, ok := result.PortalIds[pkey]; ok {
+						result.PortalIds[pkey] = append(p, pref)
 					} else {
-						result.Portals[pkey] = portal{ploc}
+						result.PortalIds[pkey] = []GridRef{pref}
 					}
-					result.Map[ploc] = mazeContent{Content: input[ploc.Y][ploc.X], PortalId: pkey}
+					if _, ok := result.Portals[pref]; !ok {
+						result.Portals[pref] = pkey
+					}
+					result.Map[pref] = mazeContent{Content: input[pref.Y][pref.X], PortalId: pkey}
 				}
 			}
 		}
